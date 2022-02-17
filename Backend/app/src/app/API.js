cef1d9301be4c90b1db33c9dabf0c277b0a5422e
Ziagl <office@ziegelwanger-edv.at>
2022-02-17 14:08:22 +0100

    added API test site

--- Backend/app/src/app/API.js
+++ Backend/app/src/app/API.js
@@ -1,4 +1,3 @@
-
 /*const api = {
     fetchJobs: async () => {
         const endpoint = "https://ziegelwanger-edv.at/autoput/api/job/read.php?pageno=1&pagesize=30";
@@ -9,4 +8,8 @@
 console.log(api.fetchJobs());*/
 
 const endpoint = "https://ziegelwanger-edv.at/autoput/api/job/read.php?pageno=1&pagesize=30";
-console.log((await fetch(endpoint)).json());
+console.log(
+    //(
+        /*await */fetch(endpoint)
+    //).json()
+);
