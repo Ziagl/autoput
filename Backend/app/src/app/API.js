38126b4793d034f7764e9efe4eb17900c7089959
Ziagl <office@ziegelwanger-edv.at>
2022-02-22 19:12:47 +0100

    api test added

--- Backend/app/src/app/API.js
+++ Backend/app/src/app/API.js
@@ -1,15 +1,33 @@
 /*const api = {
+    generateToken: async () => {
+        const endpoint = "https://ziegelwanger-edv.at/autoput/api/token/generate.php";
+        const data = {username: "admin", password: "admin123"};
+        return await (await fetch(endpoint, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+          })).json();
+    },
     fetchJobs: async () => {
         const endpoint = "https://ziegelwanger-edv.at/autoput/api/job/read.php?pageno=1&pagesize=30";
         return await (await fetch(endpoint)).json();
     }
 }
 
+console.log(api.generateToken());
 console.log(api.fetchJobs());*/
 
-const endpoint = "https://ziegelwanger-edv.at/autoput/api/job/read.php?pageno=1&pagesize=30";
-console.log(
-    //(
-        /*await */fetch(endpoint)
-    //).json()
-);
+const endpoint = "https://ziegelwanger-edv.at/autoput/api/token/generate.php";
+const data = {username: "admin", password: "admin123"};
+console.log(fetch(
+    endpoint, 
+    {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    }
+)).json();
