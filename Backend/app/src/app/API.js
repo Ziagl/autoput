2d8616df9ea7eaa6e3d083bd22417e7feffb5c47
Ziagl <office@ziegelwanger-edv.at>
2022-02-23 15:52:10 +0100

    solved api issues

--- Backend/app/src/app/API.js
+++ Backend/app/src/app/API.js
@@ -21,13 +21,19 @@ console.log(api.fetchJobs());*/
 
 const endpoint = "https://ziegelwanger-edv.at/autoput/api/token/generate.php";
 const data = {username: "admin", password: "admin123"};
-console.log(fetch(
-    endpoint, 
-    {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-    }
-)).json();
+console.log(
+    fetch(
+        endpoint, 
+        {
+            method: 'POST',
+            body: JSON.stringify(data),
+        }
+    )
+    .then(response => response.json())
+    .then(data => {
+        console.log('Success:', data);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    })
+);
